b4e47c645f263f487184bdd05b9433c4d93904db
victorhappe <viha1000@stud.ek.dk>
2025-11-20 14:33:43 +0100

    done

--- js/main.js
+++ js/main.js
@@ -1,116 +1,93 @@
+const alien = document.querySelector("#HotspotAlien");
+const mand = document.querySelector("#HotspotMand");
+const nabo = document.querySelector("#HotspotNabo");
+
 /************** HotspotAlien **************/
 
-document
-  .querySelector("#HotspotAlien")
-  .addEventListener("mouseover", mouseOverHead);
+alien.addEventListener("mouseover", mouseOverHead);
 
 function mouseOverHead() {
   console.log("mouseOverAlien");
-  document.querySelector("#HotspotAlien").style.fill = "white";
+  alien.style.fill = "white";
 }
-document
-  .querySelector("#HotspotAlien")
-  .addEventListener("mouseout", mouseOutHead);
+alien.addEventListener("mouseout", mouseOutHead);
 
 function mouseOutHead() {
   console.log("mouseOutHead");
-  document.querySelector("#HotspotAlien").style.fill = "red";
+  alien.style.fill = "red";
 }
-document.querySelector("#HotspotAlien").addEventListener("click", clickAlien);
+alien.addEventListener("click", clickAlien);
 function clickAlien() {
   console.log("clickAlien");
 
   animateBoxes();
 
-  document.querySelector(".info-text > h2").textContent = "Alien";
-  document.querySelector(".placeholder").textContent =
-    "Du har lige bortført et menneske, hvad gør du?";
-  document.querySelector(
-    "#efficiency"
-  ).innerHTML = `<h3> 1. Sikre prøven. Sørg for at mennesket kan trække vejret og ikke går i panik. (mennesker er skrøbelige og går hurtigt i stykker)</h3>`;
-  document.querySelector(
-    "#requirement"
-  ).innerHTML = `<h3>2. Berolig det. Giv det en stol, måske en snack eller noget vand. Mennesker bliver hurtigt utrykke og sultne.</h3>`;
-  document.querySelector(
-    "#reaction"
-  ).innerHTML = `<h3>2. Beslut skæbnen. Skal mennesket. A: Returneres sikker. B: Blive jeres ambassadør. Lær det universets hemmeligheder. C: Bare have en mindwipe - ingen skade sket</h3>`;
+  document.querySelector(".info-text > h2").textContent = "Du har lige beemet et menneske op, hvad gør du?";
+  document.querySelector(".placeholder").textContent = "";
+  document.querySelector("#efficiency").innerHTML = `<h3> 1. Sikre prøven. Sørg for at mennesket kan trække vejret og ikke går i panik. (mennesker er skrøbelige og går hurtigt i stykker)</h3>`;
+  document.querySelector("#requirement").innerHTML = `<h3>2. Berolig det. Giv det en stol, måske en snack eller noget vand. Mennesker bliver hurtigt utrykke og sultne.</h3>`;
+  document.querySelector("#reaction").innerHTML = `<h3>2. Beslut skæbnen. 
+  Skal mennesket, returneres sikker, blive jeres ambassadør - (Lær det universets hemmeligheder) - eller bare have en mindwipe - ingen skade sket</h3>`;
 }
 
 /************** HotspotMand **************/
 
-document
-  .querySelector("#HotspotMand")
-  .addEventListener("mouseover", mouseOverMand);
+mand.addEventListener("mouseover", mouseOverMand);
 
 function mouseOverMand() {
   console.log("mouseOverMand");
-  document.querySelector("#HotspotMand").style.fill = "white";
+  mand.style.fill = "white";
 }
 
-document
-  .querySelector("#HotspotMand")
-  .addEventListener("mouseout", mouseOutMand);
+mand.addEventListener("mouseout", mouseOutMand);
 
 function mouseOutMand() {
   console.log("mouseOutMand");
-  document.querySelector("#HotspotMand").style.fill = "red";
+  mand.style.fill = "red";
 }
 
-document.querySelector("#HotspotMand").addEventListener("click", clickMand);
+mand.addEventListener("click", clickMand);
 
 function clickMand() {
   console.log("clickMand");
 
   animateBoxes();
 
-  document.querySelector(".info-text > h2").textContent = "Mand";
-  document.querySelector(".placeholder").textContent =
-    "Du står nu på jorden og kigger op — hvad ser du?";
-  document.querySelector(
-    "#efficiency"
-  ).innerHTML = `<h3>1. Forsøg at filme det hele. Ingen tror på dig alligevel, men du skal prøve.</h3>`;
-  document.querySelector(
-    "#requirement"
-  ).innerHTML = `<h3>2. Råb på hjælp. Måske ser naboen noget!</h3>`;
-  document.querySelector("#reaction").innerHTML = `<h3>2. Beslut skæbnen</h3>`;
+  document.querySelector(".info-text > h2").textContent = "Du er lige blevet bortført af et rumvæsen, hvad gør du?";
+  document.querySelector(".placeholder").textContent = "";
+  document.querySelector("#efficiency").innerHTML = `<h3>1. Forhold dig roligt. Panik hjælper aldrig, heller ikke hvis du står foran intergalaktiske væsner. Hvis de har teknologi til at hente dig ombord, har de højst sandsynligt også styr på situationen.</h3>`;
+  document.querySelector("#requirement").innerHTML = `<h3>2. Vis ikke aggresiion. Undgå hurtige bevægelser eller truende adfærd. De kan måske tolke dig som farlig og det er næppe smart.</h3>`;
+  document.querySelector("#reaction").innerHTML = `<h3>3. Observer. Prøv at lægge mærke til deres kropssprog, lyde, teknologi, og intentioner. Ser de truende ud eller virker de nysgerrige?</h3>`;
 }
 
 /************** HotspotNabo **************/
 
-document
-  .querySelector("#HotspotNabo")
-  .addEventListener("mouseover", mouseOverNabo);
+nabo.addEventListener("mouseover", mouseOverNabo);
 
 function mouseOverNabo() {
   console.log("mouseOverNabo");
-  document.querySelector("#HotspotNabo").style.fill = "white";
+  nabo.style.fill = "white";
 }
 
-document
-  .querySelector("#HotspotNabo")
-  .addEventListener("mouseout", mouseOutNabo);
+nabo.addEventListener("mouseout", mouseOutNabo);
 
 function mouseOutNabo() {
   console.log("mouseOutNabo");
-  document.querySelector("#HotspotNabo").style.fill = "red";
+  nabo.style.fill = "red";
 }
 
-document.querySelector("#HotspotNabo").addEventListener("click", clickNabo);
+nabo.addEventListener("click", clickNabo);
 
 function clickNabo() {
   console.log("clickNabo");
 
   animateBoxes();
 
-  document.querySelector(".info-text > h2").textContent = "Nabo";
-  document.querySelector(".placeholder").textContent =
-    "Du ser din nabo blive beemet op i en UFO – hvad gør du?";
-  document.querySelector(
-    "#efficiency"
-  ).innerHTML = `<h3>1. Dokumentér det! Find din telefon og film beviset.</h3>`;
-  document.querySelector(
-    "#requirement"
-  ).innerHTML = `<h3>2. Overvej om du skal ringe til nogen… eller bare løbe!</h3>`;
+  document.querySelector(".info-text > h2").textContent = "Du ser din nabo blive beemet op i en UFO – hvad gør du?";
+  document.querySelector(".placeholder").textContent = "";
+  document.querySelector("#efficiency").innerHTML = `<h3>1. Dokumentér det! Find din telefon og film beviset.</h3>`;
+  document.querySelector("#requirement").innerHTML = `<h3>2. Overvej om du skal ringe til nogen… eller bare løbe!</h3>`;
+  document.querySelector("#reaction").innerHTML = `<h3>3. Bevar roen</h3>`;
 }
 
 /************Animation***********/
@@ -123,20 +100,14 @@ function animateBoxes() {
   document.querySelector("#reaction").classList.remove("hide");
   document.querySelector("#reaction").classList.add("fadeIn");
 
-  document
-    .querySelector("#efficiency")
-    .addEventListener("animationend", cleanup);
-  document
-    .querySelector("#requirement")
-    .addEventListener("animationend", cleanup);
+  document.querySelector("#efficiency").addEventListener("animationend", cleanup);
+  document.querySelector("#requirement").addEventListener("animationend", cleanup);
   document.querySelector("#reaction").addEventListener("animationend", cleanup);
 }
 
 function cleanup() {
   console.log("cleanup");
-  document
-    .querySelector("#reaction")
-    .removeEventListener("animationend", cleanup);
+  document.querySelector("#reaction").removeEventListener("animationend", cleanup);
   document.querySelector("#efficiency").classList.remove("fadeIn");
   document.querySelector("#requirement").classList.remove("fadeIn");
   document.querySelector("#reaction").classList.remove("fadeIn");
